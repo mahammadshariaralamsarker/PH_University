aff0cabcfd6645936f4b7d7df0e71796c32da073
mahammadshariaralam <mahammadshariaralam@gmail.com>
2024-11-26 20:56:40 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import cors from 'cors'
+import express, { Application } from 'express'
+
+const app:Application = express()
+const port = 3000
+
+app.use(express.json())
+app.use(cors())
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+// Application Routes
+
+
+export default app;
