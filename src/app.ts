501a7cacc83f5cb5debe353906ed8d353b3653b1
mahammadshariaralam <mahammadshariaralam@gmail.com>
2024-11-26 22:37:53 +0600

    setup eslint and pretier properly

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,12 @@
-import cors from 'cors'
-import express, { Application } from 'express'
+import cors from 'cors';
+import express, { Application } from 'express';
 
-const app:Application = express()
-const port = 3000
+const app: Application = express();
+app.use(express.json());
+app.use(cors());
 
-app.use(express.json())
-app.use(cors())
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
 // Application Routes
 // this is routes
-// 
+//
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 import config from './app/config/index';
-import app from "./app";
+import app from './app';
 
 async function Main() {
-    try {
-        await mongoose.connect(config.database_url as string)
-        app.listen(config.port,()=>{
-            console.log(`app running port ${config.port}`);
-        })
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    await mongoose.connect(config.database_url as string);
+    app.listen(config.port, () => {
+      console.log(`app running port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+    
+  }
 }
-
 
 Main();
