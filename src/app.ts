0277096a0a42b4c7e0931acf7c2d62870b52c02d
mahammadshariaralam <mahammadshariaralam@gmail.com>
2024-11-27 11:03:52 +0600

    add students modules

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,21 @@
 import cors from 'cors';
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
+import { StudentRoutes } from './app/modules/student/student.route';
 
 const app: Application = express();
+
+//parsers
 app.use(express.json());
 app.use(cors());
 
+// application routes
+app.use('/api/v1/students', StudentRoutes);
 
-// Application Routes
-// this is routes
-//
+const getAController = (req: Request, res: Response) => {
+  const a = 10;
+  res.send(a);
+};
+
+app.get('/', getAController);
+
 export default app;
