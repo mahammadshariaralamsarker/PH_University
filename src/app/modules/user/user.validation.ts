059f4b8d3296937b37d15a4698a1fa3e11981230
mahammadshariaralam <mahammadshariaralam@gmail.com>
2024-11-28 00:06:58 +0600

    refactor user validation, student route, controller and service

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -1,13 +1,14 @@
 import { z } from "zod";
 
 const userValidationSchema =z.object({
-  id:z.string(),
-  password:z.string().max(20,{message:"password cannot be more than 20"}),
-  needSpasswordChange:z.boolean().optional().default(true),
-  role:z.enum(['admin','students','faculty',]),
-  status:z.enum(['in-progress','blocked',]).default('in-progress'),
-  isDeleted:z.boolean().optional().default(false),
+  // id:z.string(),
+  password:z.string({invalid_type_error:'Password Must be Required'}).max(20,{message:"password cannot be more than 20"}),
+  // needSpasswordChange:z.boolean().optional().default(true),
+  // role:z.enum(['admin','students','faculty',]),
+  // status:z.enum(['in-progress','blocked',]).default('in-progress'),
+  // isDeleted:z.boolean().optional().default(false),
 })
+ 
 
 export const UserValidation = {
   userValidationSchema
