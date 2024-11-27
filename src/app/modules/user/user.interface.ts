8cd0ea007cab4dced276fa8118de8f8f46b30bef
mahammadshariaralam <mahammadshariaralam@gmail.com>
2024-11-27 22:52:02 +0600

    make userModel, interface,validation by zod

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,8 @@
+export type TUser = {
+  id:string,
+  password:string,
+  needsPasswordChange:boolean,
+  role:'admin'|'students'|'faculty',
+  status:'in-progress'|'blocked',
+  isDeleted:boolean
+}
