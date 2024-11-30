918e5b18f9778d6892e643a129483331ec69c27f
mahammadshariaralam <mahammadshariaralam@gmail.com>
2024-11-30 22:52:35 +0600

    maake validattion

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,15 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-
 import { UserService } from "./user.service";
-import { sendResponse } from "../../utils/sendResponse";
-import { RequestHandler } from "express";
+import { sendResponse } from "../../utils/sendResponse"; 
 import { catchAsync } from "../../utils/CatchAsync";
 
 
 
-const createStudent :RequestHandler= catchAsync(async (req, res) => {
+const createStudent = catchAsync(async (req, res) => {
   const {password, student: studentData } = req.body;
-  // const zodParsedData = studentValidationSchema.parse(studentData);
   const result = await UserService.createStudentIntoDB(password,studentData);
     sendResponse(res, {
     success:true,
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,8 +1,27 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { UserControllers } from './user.controller';
-
+import { AnyZodObject } from 'zod';
+import { studentValidationSchema } from '../student/student.validation';
 
 const router = express.Router();
-router.post('/create-student', UserControllers.createStudent);
+
+const validateRequest = (Schema: AnyZodObject) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await Schema.parseAsync({
+        body: req.body,
+      });
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+};
+
+router.post(
+  '/create-student',
+  validateRequest(studentValidationSchema),
+  UserControllers.createStudent,
+);
 
 export const UserRoutes = router;
