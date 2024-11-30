3a2b597836f90774c94af617f649b9709d73b456
mahammadshariaralam <mahammadshariaralam@gmail.com>
2024-11-30 22:58:43 +0600

    validate Request

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,22 +1,11 @@
-import express, { NextFunction, Request, Response } from 'express';
+import express  from 'express';
 import { UserControllers } from './user.controller';
-import { AnyZodObject } from 'zod';
 import { studentValidationSchema } from '../student/student.validation';
+import { validateRequest } from '../../middleWare/validateRequest';
 
 const router = express.Router();
 
-const validateRequest = (Schema: AnyZodObject) => {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      await Schema.parseAsync({
-        body: req.body,
-      });
-      next();
-    } catch (error) {
-      next(error);
-    }
-  };
-};
+
 
 router.post(
   '/create-student',
