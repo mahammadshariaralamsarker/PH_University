c987fc47c296dd780d9899d44aea73324439c3e1
mahammadshariaralam <mahammadshariaralam@gmail.com>
2024-11-30 21:27:47 +0600

    ..

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -2,8 +2,7 @@ export type TUser = {
   id:string,
   password:string,
   needsPasswordChange:boolean,
-  role:'admin'|'students'|'faculty',
+  role:'admin'|'students'|'faculty', 
   status:'in-progress'|'blocked',
   isDeleted:boolean
 }
-// 
