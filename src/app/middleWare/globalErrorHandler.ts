cebb6b51213efddf3cad9b03e0dccdc11f7ef5a2
mahammadshariaralam <mahammadshariaralam@gmail.com>
2024-12-06 12:05:38 +0600

    zod handle error

--- src/app/middleWare/globalErrorHandler.ts
+++ src/app/middleWare/globalErrorHandler.ts
@@ -1,21 +1,57 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-unused-vars */
 
-import { NextFunction, Request, Response } from 'express';
+import { ErrorRequestHandler } from 'express';
+import { ZodError } from 'zod';
+import config from '../config';
+// import { TErrorSource } from '../interface/error';
 
-/* eslint-disable @typescript-eslint/no-explicit-any */
-// eslint-disable-next-line no-unused-vars
-export const globalErrorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  // eslint-disable-next-line no-unused-vars
-  next: NextFunction,
+type TErrorSource = {
+  path:string|number
+  message:string
+}[]
+
+export const globalErrorHandler: ErrorRequestHandler = (
+  err,
+  req,
+  res,
+  next,
 ) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'something went wrong';
+  // setting getting values
+  let statusCode = err.statusCode || 500;
+  let message = err.message || 'Something went wrong!';
+  let errorSources:TErrorSource = [{
+    path:'',
+    message:'Error Sources message'
+  }]
+
+  const handleZodError =(err:ZodError)=>{
+    const errorSources:TErrorSource= err.issues.map((issue:ZodError)=>{
+      return{
+        path:issue?.path[issue.path.length-1],
+        message:issue.message
+      }
+    })
+    return {
+      statusCode,
+      message:'Validation Error',
+      errorSources
+    }
+  }
+
+
+  if(err instanceof ZodError){
+    const simplifiedError = handleZodError(err)
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources
+  }
+
+// ultimate Return
   return res.status(statusCode).json({
     status: false,
     message,
-    error: err,
+    errorSources,
+    stack:config.Node_Env ==='development'?err?.stack:null
   });
 };
