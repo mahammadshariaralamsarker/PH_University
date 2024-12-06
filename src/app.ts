a5c373711eb9b3e9f5b206fccd51ead5fb015225
mahammadshariaralam <mahammadshariaralam@gmail.com>
2024-12-06 19:46:41 +0600

    uncaught rejection

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,7 @@ app.use(cors());
 app.use('/api/v1', router);
 
 const getAController = (req: Request, res: Response) => {
+  // Promise.reject();
   const a = 10;
   res.send(a);
 };
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import mongoose from 'mongoose';
 import config from './app/config/index';
 import app from './app';
+import {Server} from 'http'
+
+let server : Server;
 
 async function Main() {
   try {
     await mongoose.connect(config.database_url as string);
-    app.listen(config.port, () => {
+  server= app.listen(config.port, () => {
       console.log(`app running port ${config.port}`);
     });
   } catch (error) {
@@ -14,3 +17,20 @@ async function Main() {
 }
 
 Main();
+process.on('unhandledRejection',()=>{
+  console.log("Unhandled Rejection is Rejected And Server Shutting down ");
+  if(server){
+    server.close(()=>{
+      process.exit(1)
+    })
+    
+  }
+  process.exit(1)
+})
+
+// Promise.reject();
+process.on('uncaughtException',()=>{
+  console.log("Uncaught Exception  is Rejected And Server Shutting down ");
+  process.exit(1)
+})
+console.log(z);
