cec04f6f472a2a7bddd397225ef709b1791d798a
mahammadshariaralam <mahammadshariaralam@gmail.com>
2024-12-11 23:59:04 +0600

    validation

---
+++ src/app/modules/offeredCourse/offeredCourse.validation.ts
@@ -0,0 +1,21 @@
+import { z } from "zod"
+import { Days } from "./offeredCourse.constant"
+
+
+const createOfferedCourseValidation = z.object({
+body :z.object({
+  semesterRegistration:z.string(),
+  academicSemester: z.string(),
+  academicFaculty: z.string(),
+  academicDepartment: z.string(),
+  course: z.string(),
+  faculty: z.string(),
+  maxCapacity: z.number(),
+  section:  z.number(),
+  days:z.enum([...Days] as  [string, ...string[]]),
+  startTime: z.string(),
+  endTime:z.string(),
+})
+})
+
+export const 
