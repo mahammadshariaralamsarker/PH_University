97448a8be7f8d9ea00ebec0085d4f552f0533a11
mahammadshariaralam <mahammadshariaralam@gmail.com>
2024-12-13 17:45:20 +0600

    comparison end and start time

--- src/app/modules/offeredCourse/offeredCourse.validation.ts
+++ src/app/modules/offeredCourse/offeredCourse.validation.ts
@@ -30,7 +30,14 @@ const createOfferedCourseValidation = z.object({
         message: 'Invalid Time Format , Expect "HH:MM" in 24 Hours format',
       },
     ),
-  }),
+  }).refine((body)=>{ 
+    const start = new Date(`1970-01-01T${body.startTime}:00`)
+    const end = new Date(`1970-01-01T${body.endTime}:00`)
+    return end>start
+  },
+{
+  message:'Start time should be before End time '
+}),
 });
 const updateOfferedCourseValidation = z.object({
   body: z.object({
