952bf9478d59fb0765340340194bbbdb201d45f2
mahammadshariaralam <mahammadshariaralam@gmail.com>
2024-12-16 23:34:27 +0600

    create unauthorised token access

--- src/app/middleWare/auth.ts
+++ src/app/middleWare/auth.ts
@@ -1,10 +1,15 @@
 import { NextFunction, Request, Response } from 'express';
 import { catchAsync } from '../utils/CatchAsync';
+import AppError from '../Errors/AppErrors';
+import httpstatus from "http-status-codes"
 
 export const auth = () => {
   return catchAsync(
     async (req: Request, res: Response, next: NextFunction) => {
-      console.log(req.headers.authorization);
+      const token  =req.headers.authorization
+      if(!token){
+        throw new AppError(httpstatus.UNAUTHORIZED, "You are not authorized!")
+      }
       next();
     },
   );
